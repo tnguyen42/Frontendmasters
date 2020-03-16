75e08c13f5bcb0b544c9b47ff9247637ac764317
Thanh-Quy <tnguyen@heex.io>
2020-03-16 15:40:25 +0100

    :sparkles: Incomplete exercises

--- servers-and-node/node-hard-parts/server.js
+++ servers-and-node/node-hard-parts/server.js
@@ -4,16 +4,19 @@ const fs = require('fs');
 function doOnRequest(request, response){
   // Send back a message saying "Welcome to Twitter"
   // code here...
-  response.end("yo")  
+  response.write("Welcome to Twitter")  
   if (request.method === 'GET' && request.url === '/') {
     // read the index.html file and send it back to the client
     // code here...
-    fs.createReadStream()
-
+	  fs.createReadStream("./index.html");
+	  const index = fs.readFileSync("./index.html", "utf8");
+	  response.writeHeader(200, {"Content-Type": "text/html"}); 
+	  response.write(index);
   }
   else if (request.method === 'POST' && request.url === '/sayHi') {
     // code here...
-    
+	  response.end("hi back to you!");
+	  fs.appendFileSync("hi_log.txt", "Somebody said hi.\n");
   }
   else if (request.method === 'POST' && request.url === '/greeting') {
     // accumulate the request body in a series of chunks
